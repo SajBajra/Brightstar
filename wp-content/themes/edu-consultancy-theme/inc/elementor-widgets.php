3fba2c2af9c5fbe4e80156c0e4021d9ca6d6731b
SajBajra <sajesh.bajracharya.01@gmail.com>
2026-02-16 13:19:46 +0545

    Elementor: register theme widgets

--- wp-content/themes/edu-consultancy-theme/inc/elementor-widgets.php
+++ wp-content/themes/edu-consultancy-theme/inc/elementor-widgets.php
@@ -39,6 +39,8 @@ class Edu_Theme_Elementor_Widgets {
 		require_once __DIR__ . '/elementor/class-edu-widget-company-card.php';
 		require_once __DIR__ . '/elementor/class-edu-widget-hero-jobs.php';
 		require_once __DIR__ . '/elementor/class-edu-widget-jobs-carousel.php';
+		require_once __DIR__ . '/elementor/class-edu-widget-services-carousel.php';
+		require_once __DIR__ . '/elementor/class-edu-widget-consultation-booking.php';
 
 		$widgets_manager->register( new \Edu_Elementor_Widget_Job_Grid() );
 		$widgets_manager->register( new \Edu_Elementor_Widget_Featured_Jobs() );
@@ -47,6 +49,8 @@ class Edu_Theme_Elementor_Widgets {
 		$widgets_manager->register( new \Edu_Elementor_Widget_Company_Card() );
 		$widgets_manager->register( new \Edu_Elementor_Widget_Hero_Jobs() );
 		$widgets_manager->register( new \Edu_Elementor_Widget_Jobs_Carousel() );
+		$widgets_manager->register( new \Edu_Elementor_Widget_Services_Carousel() );
+		$widgets_manager->register( new \Edu_Elementor_Widget_Consultation_Booking() );
 	}
 }
 
